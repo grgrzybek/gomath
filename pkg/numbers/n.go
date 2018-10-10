c8577f33d7f3a8f0d71010b89d3bf09e4abd5b58
Grzegorz Grzybek <gr.grzybek@gmail.com>
2018-10-10 14:27:10 +0200

    Handling power, subtraction, division, roots and logarithms in ℕ

---
+++ pkg/numbers/n.go
@@ -0,0 +1,211 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package numbers
+
+/*
+ * https://unicode-table.com/en/blocks/letterlike-symbols/
+ * Natural numbers: ℕ, u2115
+ * Integer numbers: ℤ, u2124
+ * Rational numbers: ℚ, u211A
+ * Real numbers: ℝ, u211D
+ * Complex numbers: ℂ, u2102
+ *
+ * Basic Rules for addition, multiplication and raising to a power (steming from definition):
+ * (a) a+b = b+a
+ * (d) a+(b+c) = (a+b)+c
+ * (b) a*b = b*a
+ * (c) a*(b+c) = a*b + a*c
+ * (e) (a*b)*c = a*(b*c)
+ * (f) (a*b)^c = a^c * b^c
+ * (g) a^b * a^c = a^(b+c)
+ * (h) (a^b)^c = a^(b*c)
+ * (i) a+0 = a
+ * (j) a*1 = a
+ * (k) a^1 = a
+ */
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+)
+
+var (
+	// this is the only Integer number we know initially
+	ZERO = N{value: 0}
+)
+
+// Natural numbers including ZERO
+// We suppose that we already know what integers are, what zero is, and what it means to increase a number by one unit.
+type N struct {
+	value uint64
+
+	// mark N as "implementing fmt.Stringer" using anonymous field / embedded type
+	// https://golang.org/doc/effective_go.html#embedding
+	// When we embed a type, the methods of that type become methods of the outer type, but when
+	// they are invoked the receiver of the method is the inner type, not the outer one.
+	fmt.Stringer
+	NOperations
+}
+
+type NOperations interface {
+	Add(*N) *N
+	Multiply(*N) *N
+	Power(*N) *N
+	Subtract(*N) (*N, error)
+}
+
+// Override promoted methods, by default it's just n.String() -> n.Stringer.String() and is causing NPE
+func (n *N) String() string {
+	return fmt.Sprintf("%d", n.value)
+}
+
+// "Addition": Start with integer A and increase it by 1, B times to get "A + B"
+func (n *N) Add(arg *N) *N {
+	res := n
+	for i := uint64(0); i < arg.value; i++ {
+		res = res.addOne()
+	}
+	return res
+}
+
+// "Multiplication": (requires definition of "Addition") Start with ZERO and add A to it B times to get "A * B"
+func (n *N) Multiply(arg *N) *N {
+	res := &ZERO
+	for i := uint64(0); i < arg.value; i++ {
+		res = res.Add(n)
+	}
+	return res
+}
+
+// "Raising to power": (requires definition of "Multiplication") Start with ONE and multiply it by A, B times
+// to get "A^B"
+func (n *N) Power(arg *N) *N {
+	res := NewN("1")
+	for i := uint64(0); i < arg.value; i++ {
+		res = res.Multiply(n)
+	}
+	return res
+}
+
+// "Subtraction": Assuming A and C are given, we want to find B that "A + B = C". Then B is defined as "C - A"
+func (n *N) Subtract(arg *N) (*N, error) {
+	if n.value < arg.value {
+		return nil, errors.New("no solution in \u2115")
+	}
+
+	res := &ZERO
+	for {
+		if arg.Add(res).value == n.value {
+			return res, nil
+		}
+		res = res.addOne()
+	}
+
+	return res, nil
+}
+
+// "Division": Assuming A and C are given, we want to find B that "A * B = C". Then B is defined as "C / A"
+func (n *N) Divide(arg *N) (*N, error) {
+	if arg.value == 0 {
+		return nil, errors.New("can't divide by ZERO")
+	}
+
+	res := &ZERO
+	for {
+		if arg.Multiply(res).value == n.value {
+			return res, nil
+		}
+		if arg.Multiply(res).value > n.value {
+			break
+		}
+		res = res.addOne()
+	}
+
+	return nil, errors.New("no solution in \u2115")
+}
+
+// "Root": Assuming A and C are given, we want to find B that "B ^ A = C", Then B is defined as "Ath√C".
+// n is root's degree, arg is the argument, result is a number we need to raise to power n, to get arg
+func (n *N) Root(arg *N) (*N, error) {
+	if n.value == 0 {
+		return nil, errors.New("can't take ZEROth root")
+	}
+
+	res := NewN("0")
+	for {
+		if res.Power(n).value == arg.value {
+			return res, nil
+		}
+		if res.Power(n).value > arg.value {
+			break
+		}
+		res = res.addOne()
+	}
+
+	return nil, errors.New("no solution in \u2115")
+}
+
+// "Logarithm": Assuming A and C are given, we want to find B that "A ^ B = C", Then B is defined as "log A (C)".
+// n is base, arg is the argument, result is a power to which we need to raise n, to get arg
+func (n *N) Log(arg *N) (*N, error) {
+	if arg.value == 0 {
+		return nil, errors.New("can't take logarithm from ZERO")
+	}
+	if n.value == 0 {
+		return nil, errors.New("can't take logarithm with base ZERO")
+	}
+	if n.value == 1 {
+		return nil, errors.New("can't take logarithm with base ONE")
+	}
+
+	res := NewN("0")
+	for {
+		if n.Power(res).value == arg.value {
+			return res, nil
+		}
+		if n.Power(res).value > arg.value {
+			break
+		}
+		res = res.addOne()
+	}
+
+	return nil, errors.New("no solution in \u2115")
+}
+
+// NewN Creates new N from string
+func NewN(v string) *N {
+	value, _ := strconv.Atoi(v)
+	res := &ZERO
+	for i := 0; i < value; i++ {
+		res = res.addOne()
+	}
+	return res
+}
+
+// and we only know how to "add 1" - find "next" number
+func (n *N) addOne() *N {
+	res := &N{value: n.value}
+	res.value++
+	return res
+}
+
+// validation of interface implementation
+var _ = fmt.Stringer(&N{})
+var _ = NOperations(&N{})
---
+++ pkg/numbers/n_test.go
@@ -0,0 +1,241 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package numbers
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestStringerInN(t *testing.T) {
+	var n = N{value: 42}
+	// we can access embedded interface implicitly - all that is required is "func (n *N) String() string {}"
+	fmt.Printf("%s\n", n.String())
+	// we can also access embedded interface directly, but it requires initialization
+	// fmt.Printf("%s\n", n.Stringer.String())
+	// this won't compile, since n is not an interface
+	// display(n)
+	// However this will compile!
+	// see https://golang.org/doc/effective_go.html#pointers_vs_values
+	// This rule arises because pointer methods can modify the receiver; invoking them on a value would cause the
+	// method to receive a copy of the value, so any modifications would be discarded. The language therefore
+	// disallows this mistake.
+	display(&n)
+	// this will compile as well
+	display(fmt.Stringer(&n))
+}
+
+func TestAddingOne(t *testing.T) {
+	var zero = ZERO
+	fmt.Printf("zero: %s\n", zero.String())
+	fmt.Printf("two: %s\n", zero.addOne().addOne().String())
+}
+
+func TestAdding(t *testing.T) {
+	var zero = ZERO
+	fmt.Printf("zero: %s\n", zero.String())
+	fmt.Printf("two: %s\n", zero.addOne().addOne().String())
+
+	_42 := NewN("42")
+	display(_42)
+	fmt.Printf("fourty two: %s\n", _42)
+	fmt.Printf("fourty two plus eighteen: %s\n", _42.Add(NewN("18")))
+	fmt.Printf("fourty two: %s\n", _42)
+}
+
+func TestMultiplying(t *testing.T) {
+	var zero = ZERO
+	fmt.Printf("zero: %s\n", zero.String())
+	fmt.Printf("zero x three: %s\n", zero.Multiply(NewN("3")))
+	fmt.Printf("56 x 3: %s\n", NewN("56").Multiply(NewN("3")))
+	fmt.Printf("56 x 121: %s\n", NewN("56").Multiply(NewN("121")))
+}
+
+func TestPower(t *testing.T) {
+	fmt.Printf("0^0: %s\n", ZERO.Power(&ZERO))
+	fmt.Printf("1^1: %s\n", NewN("1").Power(NewN("1")))
+	fmt.Printf("0^3: %s\n", ZERO.Power(NewN("3")))
+	fmt.Printf("3^0: %s\n", NewN("3").Power(&ZERO))
+	fmt.Printf("2^16: %s\n", NewN("2").Power(NewN("16")))
+	fmt.Printf("10^6: %s\n", NewN("10").Power(NewN("6")))
+}
+
+func TestSubtract(t *testing.T) {
+	if n, e := NewN("145").Subtract(NewN("22")); e == nil {
+		fmt.Printf("145-22: %s\n", n)
+	} else {
+		_ = fmt.Errorf("%s\n", e)
+	}
+	if n, e := NewN("42").Subtract(NewN("42")); e == nil {
+		fmt.Printf("42-42: %s\n", n)
+	} else {
+		_ = fmt.Errorf("%s\n", e)
+	}
+	if n, e := NewN("42").Subtract(NewN("43")); e == nil {
+		fmt.Printf("42-43: %s\n", n)
+	} else {
+		fmt.Printf("42-43: %s\n", fmt.Errorf("%s", e))
+	}
+}
+
+func TestDivide(t *testing.T) {
+	if n, e := NewN("140").Divide(NewN("10")); e == nil {
+		fmt.Printf("140/10: %s\n", n)
+	} else {
+		fmt.Printf("140/10: %s\n", fmt.Errorf("%s", e))
+	}
+	if n, e := NewN("140").Divide(NewN("11")); e == nil {
+		fmt.Printf("140/11: %s\n", n)
+	} else {
+		fmt.Printf("140/11: %s\n", fmt.Errorf("%s", e))
+	}
+	if n, e := NewN("42").Divide(NewN("42")); e == nil {
+		fmt.Printf("42/42: %s\n", n)
+	} else {
+		fmt.Printf("42/42: %s\n", fmt.Errorf("%s", e))
+	}
+	if n, e := NewN("0").Divide(NewN("42")); e == nil {
+		fmt.Printf("0/42: %s\n", n)
+	} else {
+		fmt.Printf("0/42: %s\n", fmt.Errorf("%s", e))
+	}
+	if n, e := NewN("1").Divide(NewN("0")); e == nil {
+		fmt.Printf("1/0: %s\n", n)
+	} else {
+		fmt.Printf("1/0: %s\n", fmt.Errorf("%s", e))
+	}
+}
+
+func TestRoot(t *testing.T) {
+	if n, e := NewN("1").Root(NewN("1")); e == nil {
+		fmt.Printf("1√1: %s\n", n)
+	} else {
+		fmt.Printf("1√1: %s\n", fmt.Errorf("%s", e))
+	}
+	if n, e := NewN("1").Root(NewN("4")); e == nil {
+		fmt.Printf("1√4: %s\n", n)
+	} else {
+		fmt.Printf("1√4: %s\n", fmt.Errorf("%s", e))
+	}
+	if n, e := NewN("4").Root(NewN("1")); e == nil {
+		fmt.Printf("4√1: %s\n", n)
+	} else {
+		fmt.Printf("4√1: %s\n", fmt.Errorf("%s", e))
+	}
+	if n, e := NewN("2").Root(NewN("9")); e == nil {
+		fmt.Printf("2√9: %s\n", n)
+	} else {
+		fmt.Printf("2√9: %s\n", fmt.Errorf("%s", e))
+	}
+	if n, e := NewN("4").Root(NewN("16")); e == nil {
+		fmt.Printf("4√16: %s\n", n)
+	} else {
+		fmt.Printf("4√16: %s\n", fmt.Errorf("%s", e))
+	}
+	if n, e := NewN("16").Root(NewN("65536")); e == nil {
+		fmt.Printf("16√65536: %s\n", n)
+	} else {
+		fmt.Printf("16√65536: %s\n", fmt.Errorf("%s", e))
+	}
+	if n, e := NewN("2").Root(NewN("65536")); e == nil {
+		fmt.Printf("2√65536: %s\n", n)
+	} else {
+		fmt.Printf("2√65536: %s\n", fmt.Errorf("%s", e))
+	}
+	if n, e := NewN("3").Root(NewN("0")); e == nil {
+		fmt.Printf("3√0: %s\n", n)
+	} else {
+		fmt.Printf("3√0: %s\n", fmt.Errorf("%s", e))
+	}
+	if n, e := NewN("0").Root(NewN("3")); e == nil {
+		fmt.Printf("0√3: %s\n", n)
+	} else {
+		fmt.Printf("0√3: %s\n", fmt.Errorf("%s", e))
+	}
+	if n, e := NewN("0").Root(NewN("0")); e == nil {
+		fmt.Printf("0√0: %s\n", n)
+	} else {
+		fmt.Printf("0√0: %s\n", fmt.Errorf("%s", e))
+	}
+}
+
+func TestLogarithm(t *testing.T) {
+	if n, e := NewN("1").Log(NewN("1")); e == nil {
+		fmt.Printf("log 1 (1): %s\n", n)
+	} else {
+		fmt.Printf("log 1 (1): %s\n", fmt.Errorf("%s", e))
+	}
+	if n, e := NewN("1").Log(NewN("4")); e == nil {
+		fmt.Printf("log 1 (4): %s\n", n)
+	} else {
+		fmt.Printf("log 1 (4): %s\n", fmt.Errorf("%s", e))
+	}
+	if n, e := NewN("4").Log(NewN("1")); e == nil {
+		fmt.Printf("log 4 (1): %s\n", n)
+	} else {
+		fmt.Printf("log 4 (1): %s\n", fmt.Errorf("%s", e))
+	}
+	if n, e := NewN("2").Log(NewN("9")); e == nil {
+		fmt.Printf("log 2 (9): %s\n", n)
+	} else {
+		fmt.Printf("log 2 (9): %s\n", fmt.Errorf("%s", e))
+	}
+	if n, e := NewN("3").Log(NewN("9")); e == nil {
+		fmt.Printf("log 3 (9): %s\n", n)
+	} else {
+		fmt.Printf("log 3 (9): %s\n", fmt.Errorf("%s", e))
+	}
+	if n, e := NewN("4").Log(NewN("16")); e == nil {
+		fmt.Printf("log 4 (16): %s\n", n)
+	} else {
+		fmt.Printf("log 4 (16): %s\n", fmt.Errorf("%s", e))
+	}
+	if n, e := NewN("16").Log(NewN("65536")); e == nil {
+		fmt.Printf("log 16 (65536): %s\n", n)
+	} else {
+		fmt.Printf("log 16 (65536): %s\n", fmt.Errorf("%s", e))
+	}
+	if n, e := NewN("2").Log(NewN("65536")); e == nil {
+		fmt.Printf("log 2 (65536): %s\n", n)
+	} else {
+		fmt.Printf("log 2 (65536): %s\n", fmt.Errorf("%s", e))
+	}
+	if n, e := NewN("3").Log(NewN("0")); e == nil {
+		fmt.Printf("log 3 (0): %s\n", n)
+	} else {
+		fmt.Printf("log 3 (0): %s\n", fmt.Errorf("%s", e))
+	}
+	if n, e := NewN("0").Log(NewN("3")); e == nil {
+		fmt.Printf("log 0 (3): %s\n", n)
+	} else {
+		fmt.Printf("log 0 (3): %s\n", fmt.Errorf("%s", e))
+	}
+	if n, e := NewN("0").Log(NewN("0")); e == nil {
+		fmt.Printf("log 0 (0): %s\n", n)
+	} else {
+		fmt.Printf("log 0 (0): %s\n", fmt.Errorf("%s", e))
+	}
+}
+
+func display(s fmt.Stringer) {
+	// fmt.Printf("%s\n", s.String())
+	// fmt.Printf can handle fmt.Stringer interface
+	fmt.Printf("%s\n", s)
+	// glog.Info(s)
+}
